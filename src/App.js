dc86e0232aa1695c750a48f629095dda2fbafa26
SANKAR2167 <snakarkulandaivel@gmail.com>
2022-11-23 11:06:11 +0530

    q

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <div className='container'>
           <div className='row'>
             <div className='col-md-4'>
-              <div className='sm'>
+              <div className="card border-primary mb-3" style={"max-width: 25rem;"}>
                 <h6>FREE</h6>
                 <h2>$0 <span className='month'><sub>/month</sub></span></h2>
                 <br/>
