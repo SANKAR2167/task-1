758eb5744f3532243f88e833690c46f2c40d8ec4
SANKAR2167 <snakarkulandaivel@gmail.com>
2022-11-23 11:18:18 +0530

    H

--- src/App.js
+++ src/App.js
@@ -34,8 +34,30 @@ function App() {
             </div>
             <div className='col-md-4'>
               <div className='sm'>
-                <h6>FREE</h6>
-                <h2>$0 <span className='month'><sub>/month</sub></span></h2>
+                <h6>PLUS</h6>
+                <h2>$9 <span className='month'><sub>/month</sub></span></h2>
+                <br/>
+                <hr/>
+                <ul class="fa-ul">
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>5 Users</strong></li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>50GB Storage</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Free Subdomain</li>
+                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
+                    Reports</li>
+                </ul>
+                <div class="d-grid">
+                  <a href="#" class="btn btn-primary text-uppercase">Button</a>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <div className='sm'>
+                <h6>PLUS</h6>
+                <h2>$9 <span className='month'><sub>/month</sub></span></h2>
                 <br/>
                 <hr/>
                 <ul class="fa-ul">
