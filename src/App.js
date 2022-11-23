15294b3c5b084f6a953fea2b6a3c28de27ec2574
SANKAR2167 <snakarkulandaivel@gmail.com>
2022-11-23 10:59:43 +0530

    g

--- src/App.js
+++ src/App.js
@@ -10,9 +10,23 @@ function App() {
             <div className='col-md-6'>
               <div className='sm'>
                 <h6>FREE</h6>
-                <h2>$0 <span><sub>/month</sub></span></h2>
+                <h2>$0 <span className='month'><sub>/month</sub></span></h2>
                 <br/>
                 <hr/>
+                <ul className='fa-ul'>
+                <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited
+                Private Projects</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated
+                Phone Support</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain
+              </li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
+                Reports</li>
+                </ul>
               </div>
             </div>
           </div>
