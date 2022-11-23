fde96f216de662e794374f79c76328715d9d0618
SANKAR2167 <snakarkulandaivel@gmail.com>
2022-11-23 11:15:17 +0530

    f

--- src/App.js
+++ src/App.js
@@ -14,19 +14,22 @@ function App() {
                 <br/>
                 <hr/>
                 <ul className='fa-ul'>
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited
-                Private Projects</li>
-              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated
-                Phone Support</li>
-              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain
-              </li>
-              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
-                Reports</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited
+                    Private Projects</li>
+                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated
+                    Phone Support</li>
+                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain
+                  </li>
+                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
+                    Reports</li>
                 </ul>
+                <div class="d-grid">
+                  <a href="#" class="btn btn-primary text-uppercase">Button</a>
+                </div>
               </div>
             </div>
             <div className='col-md-4'>
@@ -36,16 +39,19 @@ function App() {
                 <br/>
                 <hr/>
                 <ul class="fa-ul">
-              <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>5 Users</strong></li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>50GB Storage</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Free Subdomain</li>
-              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
-                Reports</li>
-            </ul>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>5 Users</strong></li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>50GB Storage</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Free Subdomain</li>
+                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
+                    Reports</li>
+                </ul>
+                <div class="d-grid">
+                  <a href="#" class="btn btn-primary text-uppercase">Button</a>
+                </div>
               </div>
             </div>
           </div>
