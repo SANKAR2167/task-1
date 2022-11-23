5d18a6f9e666e267cb9c869f8c43e788233cebaa
SANKAR2167 <snakarkulandaivel@gmail.com>
2022-11-23 11:40:09 +0530

    g

--- src/App.js
+++ src/App.js
@@ -11,26 +11,26 @@ function App() {
               <div className='sm'>
               <div  className='header'>
                 <h6>FREE</h6>
-                <h2>$0 <span><sub>/month</sub></span></h2>
+                <h2 className='name'>$0 <span><sub>/month</sub></span></h2>
                 </div>
                 <br/>
                 <hr/>
                 <ul className='fa-ul'>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span>Single User</li>
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span>5GB Storage</li>
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
+                  <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Unlimited
                     Private Projects</li>
-                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated
+                  <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Dedicated
                     Phone Support</li>
-                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain
+                  <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Free Subdomain
                   </li>
-                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
+                  <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status
                     Reports</li>
                 </ul>
-                <div class="d-grid">
-                  <a href="#" class="btn btn-primary text-uppercase">Button</a>
+                <div className="d-grid">
+                  <a href="#" className="btn btn-primary text-uppercase">Button</a>
                 </div>
               </div>
             </div>
@@ -42,19 +42,19 @@ function App() {
                 </div>
                 <br/>
                 <hr/>
-                <ul class="fa-ul">
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>5 Users</strong></li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>50GB Storage</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Free Subdomain</li>
-                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
+                <ul className="fa-ul">
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>5 Users</strong></li>
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span>50GB Storage</li>
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Private Projects</li>
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span>Dedicated Phone Support</li>
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span>Free Subdomain</li>
+                  <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status
                     Reports</li>
                 </ul>
-                <div class="d-grid">
-                  <a href="#" class="btn btn-primary text-uppercase">Button</a>
+                <div className="d-grid">
+                  <a href="#" className="btn btn-primary text-uppercase">Button</a>
                 </div>
               </div>
             </div>
@@ -66,20 +66,20 @@ function App() {
                 </div>
                 <br/>
                 <hr/>
-                <ul class="fa-ul">
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited Users</strong>
+                <ul className="fa-ul">
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>Unlimited Users</strong>
                   </li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>150GB Storage</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited</strong> Free
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span>150GB Storage</li>
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Private Projects</li>
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span>Dedicated Phone Support</li>
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>Unlimited</strong> Free
                     Subdomains</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Monthly Status Reports</li>
+                  <li><span className="fa-li"><i className="fas fa-check"></i></span>Monthly Status Reports</li>
                 </ul>
-                <div class="d-grid">
-                  <a href="#" class="btn btn-primary text-uppercase">Button</a>
+                <div className="d-grid">
+                  <a href="#" className="btn btn-primary text-uppercase">Button</a>
                 </div>
               </div>
             </div>
