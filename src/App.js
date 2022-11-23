257f6f5358cad976e191f5ff7a9a165da9214f9e
SANKAR2167 <snakarkulandaivel@gmail.com>
2022-11-23 11:20:34 +0530

    CONTENT GET FINISHED

--- src/App.js
+++ src/App.js
@@ -56,20 +56,21 @@ function App() {
             </div>
             <div className='col-md-4'>
               <div className='sm'>
-                <h6>PLUS</h6>
-                <h2>$9 <span className='month'><sub>/month</sub></span></h2>
+                <h6>PRO</h6>
+                <h2>$49 <span className='month'><sub>/month</sub></span></h2>
                 <br/>
                 <hr/>
                 <ul class="fa-ul">
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>5 Users</strong></li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>50GB Storage</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited Users</strong>
+                  </li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>150GB Storage</li>
                   <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
                   <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
                   <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
                   <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Free Subdomain</li>
-                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
-                    Reports</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited</strong> Free
+                    Subdomains</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Monthly Status Reports</li>
                 </ul>
                 <div class="d-grid">
                   <a href="#" class="btn btn-primary text-uppercase">Button</a>
